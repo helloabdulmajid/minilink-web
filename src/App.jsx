ca2b4115ccc3da4b9c008b3f34cad84c5670d06b
Abdul Majid <helloabdulmajid@gmail.com>
2026-05-21 13:59:38 +0530

    feat: add backend wake-up request on app load

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react"
 import { Routes, Route } from "react-router-dom"
 
 import HomePage from "./pages/HomePage"
 import AnalyticsPage from "./pages/AnalyticsPage"
 
 function App() {
+
+  useEffect(() => {
+
+    fetch("https://minilink-twtn.onrender.com/health")
+      .catch(() => {})
+
+  }, [])
 
   return (
 
